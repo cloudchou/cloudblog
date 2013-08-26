c2f179beafe7fcd54dfad482a6ed72b500f09f62
Cloud Chou <515312382@qq.com>
2013-08-27 00:49:27 +0800

    完成分类页面设计 并添加友言评论插件，并未完成

---
+++ wp-content/plugins/youyan-social-comment-system/comment.php
@@ -0,0 +1,6 @@
+<?php 
+
+echo stripslashes(get_option('uyan_src'));
+
+?>
+
--- wp-content/themes/cloudlove2/header-header.php
+++ wp-content/themes/cloudlove2/header-header.php
@@ -112,33 +112,5 @@ if ( is_single() ){
 </div>
 <div id="content">
 <div class="position">
-<?php if ( !is_home() && ! is_page() ) {  ?>
-当前位置：
-<a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
- > 
-
-<?php
-if( is_single() ){
-$categorys = get_the_category();
-$category = $categorys[0];
-echo is_wp_error( $category_parents = get_category_parents($category->term_id,true,' >') ) ? '' : $category_parents;echo' 查看文章';
-} elseif ( is_page() ){
-the_title();
-} elseif ( is_category() ){
-single_cat_title();
-} elseif ( is_tag() ){
-single_tag_title();
-} elseif ( is_day() ){
-the_time('Y年Fj日');
-} elseif ( is_month() ){
-the_time('Y年F');
-} elseif ( is_year() ){
-the_time('Y年');
-} elseif ( is_search() ){
-echo $s.' 的搜索结果';
-}
-else
-{}
-?>
-<?php }?>
+ 
 </div>
