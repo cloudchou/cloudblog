eaa820155999b39584b31b7f096ca6cde7f05a23
Cloud Chou <515312382@qq.com>
2014-04-22 23:48:40 +0800

    添加CNZZ统计

--- wp-content/themes/cloudlove/footer.php
+++ wp-content/themes/cloudlove/footer.php
@@ -31,12 +31,17 @@ $(document).ready(function(){
 </div>
 
 <div id="footer">
-<div style="display:none">
-<script type="text/javascript">
-var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F50761bb5ef303074c39d254b24132503' type='text/javascript'%3E%3C/script%3E"));
-</script>
-</div>
+	<div style="display: none">
+		<script type="text/javascript">
+			var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+			document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F50761bb5ef303074c39d254b24132503' type='text/javascript'%3E%3C/script%3E"));
+		</script>
+	</div>
+	<div style="display: none;">
+		<script type="text/javascript">
+	 	var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1000385810'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "v1.cnzz.com/z_stat.php%3Fid%3D1000385810' type='text/javascript'%3E%3C/script%3E"));
+		</script>
+	</div>
 		Copyright &copy; <?php bloginfo('name'); ?> |  Theme modifyed by Cloud  | Theme by TangStyle | Powered by WordPress.
 	</div>
 
