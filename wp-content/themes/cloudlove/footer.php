60d789fe75677ce5e632ffaf59f7928bdb50cb4b
cloud <515312382@qq.com>
2014-06-05 13:15:03 +0800

    添加google统计

--- wp-content/themes/cloudlove/footer.php
+++ wp-content/themes/cloudlove/footer.php
@@ -42,6 +42,16 @@ $(document).ready(function(){
 	 	var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1000385810'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "v1.cnzz.com/z_stat.php%3Fid%3D1000385810' type='text/javascript'%3E%3C/script%3E"));
 		</script>
 	</div>
+	<div style="display: none;">
+   <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-51653888-1', 'cloudchou.com');
+  ga('send', 'pageview');
+</script> 
+    </div>
 		Copyright &copy; <?php bloginfo('name'); ?> |  Theme modifyed by Cloud  | Theme by TangStyle | Powered by WordPress.
 	</div>
 
